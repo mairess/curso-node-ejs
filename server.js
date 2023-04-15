e35c169b389cbccabf0e1f1bc43a410d04b95a83
mairess <trabalho.maires@gmail.com>
2023-04-15 17:52:57 -0300

    update server with new array

--- server.js
+++ server.js
@@ -4,7 +4,38 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-    res.render('pages/index');
+    const items = [
+        { 
+            title: 'D',
+            message: 'esenvolver aplicações/serviços de forma fácil'
+        },
+        {
+            title: 'E',
+            message: 'JS usa JavaScript para rederizar HTML'
+        },
+        {
+            title: 'M',
+            message: 'uito fácil de usar'
+        },
+        {
+            title: 'A',
+            message: 'morzinho'
+        },
+        {
+            title: 'I',
+            message: 'nstall ejs'
+        },
+        {
+            title: 'S',
+            message: 'intaxe simples'
+        },
+    ];
+
+    const subtitle = 'Uma linguagem de modelagem para criação de páginas HTML utilizando JavaScript'
+    res.render('pages/index', {
+        qualities: items,
+        subtitle: subtitle,
+    });
 })
 
 app.get('/sobre', function(req, res){
